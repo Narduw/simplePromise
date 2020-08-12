d4fcdd0f69ecb9118da3e9b45ea36b0e289654be
Bernardo Nunes <bernardo.nunes@realdolmen.com>
2020-08-12 09:24:07 +0200

    Some code cleanup

--- simplePromise.js
+++ simplePromise.js
@@ -6,7 +6,7 @@ const PROMISE_STATE = {
 
 export default function SimplePromise (task) {
     if (!task || typeof task !== 'function') throw new Error('SimplePromise must receive a task');
-    
+
     this.state = PROMISE_STATE.PENDING;
     this.value = null;
     this.callbackQueue = [];
@@ -19,25 +19,27 @@ export default function SimplePromise (task) {
 
         // if thennable, call then() function in order to chain other promises
         if (value && value.then && typeof value.then === 'function') {
-            return value.then(this.resolve.bind(this), this.reject.bind(this));
+            value.then(this.resolve.bind(this), this.reject.bind(this));
+            return;
         }
 
         this.state = state;
         this.value = value;
+
         runCallbacksAsync();
     }
 
-    // run all callbacks for this promise asynchronously
-    const runCallbacksAsync = () => this.callbackQueue.forEach(({ onResolve, onReject }) => {
+    const runCallbacksAsync = () => this.callbackQueue.forEach(handlers => {
+        // run all callbacks for this promise asynchronously
         setTimeout(() => {
-            if (this.state === PROMISE_STATE.RESOLVED) {
-                return onResolve(this.value);
-            }
-            if (this.state === PROMISE_STATE.REJECTED) {
-                return onReject(this.value);
-            }
+            runCallback.call(this, handlers);
         }, 0);
     });
+
+    const runCallback = ({ onResolve, onReject }) => {
+        if (this.state === PROMISE_STATE.RESOLVED) return onResolve(this.value);
+        if (this.state === PROMISE_STATE.REJECTED) return onReject(this.value);
+    }
     
     this.resolve = setState(PROMISE_STATE.RESOLVED);
     this.reject = setState(PROMISE_STATE.REJECTED);
@@ -55,31 +57,32 @@ export default function SimplePromise (task) {
     this.then = function(resolveCallback, rejectCallback) {
         const that = this;
         return new SimplePromise((resolve, reject) => {
-            const onResolve = value => setTimeout(() => {
-                try {
-                    if (!resolveCallback) return resolve(value);
-                    resolve(resolveCallback(value));
-                } catch(err) {
-                    reject(err);
-                }
-            }, 0);
-            const onReject = value => setTimeout(() => {
-                try {
-                    if (!rejectCallback) return reject(value);
-                    resolve(rejectCallback(value));
-                } catch(otherError) {
-                    reject(otherError);
-                }
-            }, 0);
+            const handlers = {
+                onResolve: value => setTimeout(() => {
+                    try {
+                        if (!resolveCallback) return resolve(value);
+                        resolve(resolveCallback(value));
+                    } catch(err) {
+                        reject(err);
+                    }
+                }, 0),
+                onReject: value => setTimeout(() => {
+                    try {
+                        // reject if there is no error handler, reject
+                        if (!rejectCallback) return reject(value);
+                        resolve(rejectCallback(value));
+                    } catch(otherError) {
+                        reject(otherError);
+                    }
+                }, 0)
+            };
+
+            if (that.state === PROMISE_STATE.PENDING) return that.callbackQueue.push(handlers);
 
             // call handlers directly if previous promise is already fulfilled
-            if (that.state === PROMISE_STATE.RESOLVED) return onResolve(that.value);
-            if (that.state === PROMISE_STATE.REJECTED) return onReject(that.value);
-            that.callbackQueue.push({ onResolve, onReject });
+            runCallback.call(that, handlers);
         });
     }
 
-    this.catch = rejectCallback => this.then(noop, rejectCallback);
+    this.catch = rejectCallback => this.then(null, rejectCallback);
 };
-
-function noop(value) { return value; }
